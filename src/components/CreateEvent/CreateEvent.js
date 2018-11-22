8f502172b84f202902f41d63ebb33a7f1cc53a64
ianlikono <ianlikono@gmail.com>
2018-11-21 18:30:54 -0600

    started up

---
+++ src/components/CreateEvent/CreateEvent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+    <div>
+        <h3>New Event</h3>
+    </div>
+)
--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
+import CreateEvent from "./components/CreateEvent/CreateEvent";
 import EventDetail from "./components/EventDetail/EventDetail";
 import Home from "./components/Home/Home";
 
@@ -7,5 +8,6 @@ export default (
   <Switch>
     <Route path="/" exact component={Home} />
     <Route path="/event/:id" exact component={EventDetail} />
+    <Route path="/new" exact component={CreateEvent} />
   </Switch>
 );
